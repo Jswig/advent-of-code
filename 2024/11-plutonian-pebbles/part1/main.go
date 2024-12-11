108037060b25fe58fa0de3d4a0cfed298a85fce6
Anders Poirel <andersjopo@gmail.com>
2024-12-11 15:32:47 +0100

    part 1

--- 2024/11-plutonian-pebbles/part1/main.go
+++ 2024/11-plutonian-pebbles/part1/main.go
@@ -6,8 +6,36 @@ import (
 	"strings"
 )
 
+func hasEvenNumberDigits(k int) bool {
+	if (len(strconv.Itoa(k)) % 2) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func splitDigits(k int) []int {
+	digits := strconv.Itoa(k)
+	midPoint := len(digits) / 2
+	first, _ := strconv.Atoi(digits[:midPoint])
+	second, _ := strconv.Atoi(digits[midPoint:])
+	return []int{first, second}
+}
+
 func blink(stones []int) []int {
-	return stones
+	// we know that the new stones will be at least the length of the existing stones
+	// so we allocate the an array of that size upfront
+	newStones := make([]int, 0, len(stones))
+	for _, stone := range stones {
+		if stone == 0 {
+			newStones = append(newStones, 1)
+		} else if hasEvenNumberDigits(stone) {
+			newStones = append(newStones, splitDigits(stone)...)
+		} else {
+			newStones = append(newStones, stone*2024)
+		}
+	}
+	return newStones
 }
 
 func blinkSeveral(stones []int, times int) []int {
@@ -26,19 +54,11 @@ func parseStones(text string) []int {
 	return parsedStones
 }
 
-func formatStones(stones []int) string {
-	unparsedStones := make([]string, len(stones))
-	for i, stone := range stones {
-		unparsedStones[i] = strconv.Itoa(stone)
-	}
-	return strings.Join(unparsedStones, " ")
-}
-
 func main() {
 	input := "814 1183689 0 1 766231 4091 93836 46"
-	times := 25
+	times := 75
 	orginalStones := parseStones(input)
 	finalStones := blinkSeveral(orginalStones, times)
-	result := formatStones(finalStones)
-	fmt.Printf("Stones after blinking %d times:\n%v", times, result)
+	result := len(finalStones)
+	fmt.Printf("Number of stones after blinking %d times: %d\n", times, result)
 }
